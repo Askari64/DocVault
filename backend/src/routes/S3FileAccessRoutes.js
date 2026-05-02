b515a76eeed570eaaa718e8adb1ad715a3bb9af7
Askari <askarir64@gmail.com>
2026-05-02 12:34:06 +0530

    added require auth middleware

--- backend/src/routes/S3FileAccessRoutes.js
+++ backend/src/routes/S3FileAccessRoutes.js
@@ -4,9 +4,9 @@ import { requireAuth } from "@clerk/express";
 
 const router = express.Router();
 
-router.use(requireAuth)
+router.use(requireAuth());
 
 // Route: POST /api/S3FileAccess/upload-url
-router.post("/upload-url",generateUploadUrl)
+router.post("/upload-url", generateUploadUrl);
 
 export default router;
