c0f3fe4f57602f4dbdb0b38e281f61277088e0ac
Askari <askarir64@gmail.com>
2026-05-02 12:35:18 +0530

    integrate /api/documents route

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,9 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { connectDB, disconnectDB } from "./config/db.js";
-import { clerkMiddleware, clerkClient, getAuth } from "@clerk/express"
-import S3FileAccessRoutes from "./routes/S3FileAccessRoutes.js"
+import { clerkMiddleware, clerkClient, getAuth } from "@clerk/express";
+import S3FileAccessRoutes from "./routes/S3FileAccessRoutes.js";
+import DocumentUploadRoutes from "./routes/DocumentRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -12,37 +13,47 @@ const app = express();
 connectDB();
 
 //2. CORS Configuration
-app.use(cors({
-  origin: ["http://127.0.0.1:5173", "http://localhost:5173"]/*We will change this to frontend url */,
-  methods: ["GET", "POST", "PATCH", "DELETE"] /*Allowing only these methods */,
-  credentials: true /*Allowing cookies */,
-}))
+app.use(
+  cors({
+    origin: [
+      "http://127.0.0.1:5173",
+      "http://localhost:5173",
+    ] /*We will change this to frontend url */,
+    methods: [
+      "GET",
+      "POST",
+      "PATCH",
+      "DELETE",
+    ] /*Allowing only these methods */,
+    credentials: true /*Allowing cookies */,
+  }),
+);
 
 // 3. Body Parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded());
 
 //4. Authentication Middleware (Clerk Middleware will go here)
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 //5. API Routes will go here
 
 app.use("/api/S3FileAccess", S3FileAccessRoutes);
+app.use("/api/documents", DocumentUploadRoutes);
 
 // Use `getAuth()` to protect a route based on authentication status
-app.get('/protected' ,(req, res) => {
-  const auth = getAuth(req)
+app.get("/protected", (req, res) => {
+  const auth = getAuth(req);
 
   if (!auth.isAuthenticated) {
-    return res.status(401).send('User not authenticated')
+    return res.status(401).send("User not authenticated");
   }
 
-  return res.json(auth)
-})
+  return res.json(auth);
+});
 
-app.get('/', (req,res) => {
-  res.send('Server running')
-})
-
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
