6831c8b3b1dd1ab6921d871f4157c175bdb73ab9
Askari <askarir64@gmail.com>
2026-05-06 20:00:09 +0530

    WIP routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router";
+import LandingPage from "./pages/LandingPage";
+import NotFound from "./pages/NotFound";
+import DocumentList from "./pages/DocumentList";
+import Uploader from "./pages/Uploader";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/documents" element={<DocumentList />} />
+        <Route path="/upload" element={<Uploader />} />
+        {/* 404-Not Found Page */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
