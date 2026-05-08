8cf2f6f877dbe07506cf824756315b79dbc3d1bc
Askari <askarir64@gmail.com>
2026-05-08 14:44:40 +0530

    resolved import

--- frontend/src/utils/ScrollToTop.jsx
+++ frontend/src/utils/ScrollToTop.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router";
 
 /**
  * @behaviour - Scrolls to top of page on changing url path
