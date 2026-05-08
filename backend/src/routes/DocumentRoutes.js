0f2e6fffbd842c4cec3cecb520eec107b3b51149
Askari <askarir64@gmail.com>
2026-05-08 09:53:35 +0530

    removed requireAuth - not needed

--- backend/src/routes/DocumentRoutes.js
+++ backend/src/routes/DocumentRoutes.js
@@ -1,10 +1,8 @@
 import express, { Router } from "express";
-import { requireAuth } from "@clerk/express";
 import { saveDocument } from "../controllers/DocumentController.js";
 
 const router = new express.Router();
 
-router.use(requireAuth());
 
 // Route: POST /api/documents
 // Triggered ONLY after the frontend successfully uploads to S3
--- backend/src/routes/S3FileAccessRoutes.js
+++ backend/src/routes/S3FileAccessRoutes.js
@@ -1,10 +1,8 @@
 import express from "express";
 import { generateUploadUrl, generateDownloadUrl } from "../controllers/S3FileAccessController.js";
-import { requireAuth } from "@clerk/express";
 
 const router = express.Router();
 
-router.use(requireAuth());
 
 // Route: POST /api/S3FileAccess/upload-url
 router.post("/upload-url", generateUploadUrl);
