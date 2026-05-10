5a303c6aca619e6af0f19b9a49290b87356ef7be
Askari <askarir64@gmail.com>
2026-05-10 23:23:45 +0530

    shadcn added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from "node:url"; // <-- 1. Import this instead of 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,10 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      // 2. Use import.meta.url (which is completely safe in "type": "module")
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
