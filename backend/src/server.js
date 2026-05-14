3af4753265783d5d44cdc3b0c5b80683ea3e5380
Askari <askarir64@gmail.com>
2026-05-14 20:47:02 +0530

    cors url update

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:5173",
+      "https://doc-vault-dun.vercel.app",
       "http://localhost:5173",
     ] /*We will change this to frontend url */,
     methods: [
